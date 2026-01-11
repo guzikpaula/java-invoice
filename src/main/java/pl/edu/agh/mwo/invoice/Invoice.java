91fac917f51d4b674224a81a394e85f8163a785b
pguzik <guzikpaula@gmail.com>
2026-01-11 15:54:16 +0100

    Invoice tests

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,52 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
 
-    public void addProduct(Product product) {
-        // TODO: implement
-    }
+    private Map<Product, Integer>
+    products = new HashMap<>();
 
-    public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
-    }
+public void addProduct(Product product ){
+  this.addProduct(product, 1);
+       }
 
-    public BigDecimal getSubtotal() {
-        return null;
+   public void addProduct(Product product, Integer quantity) {
+        this.products.put(product, quantity);
+      }
+
+    public BigDecimal getNetValue() {
+        BigDecimal value = BigDecimal.ZERO;
+
+        for (Product product : this.products.keySet()) {
+            Integer quantity = this.products.get(product);
+            BigDecimal price = product.getPrice();
+            price = price.multiply(BigDecimal.valueOf(quantity));
+           value = value.add(product.getPrice());
+       }
+        return value;
     }
 
     public BigDecimal getTax() {
-        return null;
+        return getGrossValue().subtract(getNetValue());
     }
 
-    public BigDecimal getTotal() {
-        return null;
+    public BigDecimal getGrossValue() {
+
+        BigDecimal value = BigDecimal.ZERO;
+
+        for (Product product : this.products.keySet()) {
+            Integer quantity = this.products.get(product);
+            BigDecimal price = product.getPriceWithTax().subtract(product.getPrice());
+            price = price.multiply(BigDecimal.valueOf(quantity));
+            value = value.add(product.getPrice());
+        }
+        return value;
     }
 }
