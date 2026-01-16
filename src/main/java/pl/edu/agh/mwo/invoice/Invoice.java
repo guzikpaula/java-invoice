604a663b2e100a312caa8f8e05c6b1fb53021258
guzikpaula <guzikpaula@gmail.com>
2026-01-16 22:10:19 +0100

    Invoice tests complete

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -18,34 +18,47 @@ public void addProduct(Product product ){
        }
 
    public void addProduct(Product product, Integer quantity) {
-        this.products.put(product, quantity);
+       if (quantity <= 0) {
+           throw new IllegalArgumentException("Quantity must be positive");
+       }
+
+       if (product == null ) {
+           throw new IllegalArgumentException("Product cannot be empty");
+       }
+
+    this.products.put(product, quantity);
       }
 
     public BigDecimal getNetValue() {
         BigDecimal value = BigDecimal.ZERO;
 
-        for (Product product : this.products.keySet()) {
-            Integer quantity = this.products.get(product);
-            BigDecimal price = product.getPrice();
-            price = price.multiply(BigDecimal.valueOf(quantity));
-           value = value.add(product.getPrice());
-       }
+        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+            Product product = entry.getKey();
+            int quantity = entry.getValue();
+
+            BigDecimal price = product.getPrice()
+                    .multiply(BigDecimal.valueOf(quantity));
+
+            value = value.add(price);
+        }
         return value;
     }
 
     public BigDecimal getTax() {
-        return getGrossValue().subtract(getNetValue());
+    return getGrossValue().subtract(getNetValue());
     }
 
     public BigDecimal getGrossValue() {
-
         BigDecimal value = BigDecimal.ZERO;
 
-        for (Product product : this.products.keySet()) {
-            Integer quantity = this.products.get(product);
-            BigDecimal price = product.getPriceWithTax().subtract(product.getPrice());
-            price = price.multiply(BigDecimal.valueOf(quantity));
-            value = value.add(product.getPrice());
+        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+            Product product = entry.getKey();
+            int quantity = entry.getValue();
+
+            BigDecimal grossPrice = product.getPriceWithTax()
+                    .multiply(BigDecimal.valueOf(quantity));
+
+            value = value.add(grossPrice);
         }
         return value;
     }
